c6c597c66307de8fb7076ec94b18fe3f2fa27d37
amaticvitez <46754662+amaticvitez@users.noreply.github.com>
2019-01-19 12:21:48 +0100

    1

--- monovitez/monovitez/Controllers/HomeController.cs
+++ monovitez/monovitez/Controllers/HomeController.cs
@@ -5,17 +5,22 @@ using System.Web;
 using System.Web.Mvc;
 using monovitez.DAL;
 using monovitez.DAL.FactoryClasses;
+using monovitez.DAL.EntityClasses;
 
 
 namespace monovitez.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
+            VehicleMakeEntity v = new VehicleMakeEntity();
+          
+
             //var cars  = f.VehicleMake.Field.
 
-            return View();
+            return View(v);
         }
     }
 }
