55adca2928761912d23528aff9fc1c49d6b9b7cb
amaticvitez <46754662+amaticvitez@users.noreply.github.com>
2019-01-19 12:10:57 +0100

    dal

--- monovitez/monovitez/Controllers/HomeController.cs
+++ monovitez/monovitez/Controllers/HomeController.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using monovitezEnt;
-using monovitezEnt.FactoryClasses;
+using monovitez.DAL;
+using monovitez.DAL.FactoryClasses;
+
 
 namespace monovitez.Controllers
 {
     public class HomeController : Controller
     {
-
-        QueryFactory f = new QueryFactory();
-
         public ActionResult Index()
         {
             //var cars  = f.VehicleMake.Field.
